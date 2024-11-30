3b4da011d5befb815d9feace1704167c37b21d12
Андрей Пустовит <andreyandreevgr@gmail.com>
2024-12-01 02:48:20 +0300

    fixed bug

--- client/src/features/file-picker/ui/FileTreePage.tsx
+++ client/src/features/file-picker/ui/FileTreePage.tsx
@@ -122,7 +122,7 @@ export const FileTreePage: React.FC = () => {
 
     try {
       setFileLoadingStatus((prev) => ({ ...prev, [file.key]: true }));
-      const response = await fetch('http://localhost:3002/api/lint', {
+      const response = await fetch('http://localhost:3000/api/lint', {
         method: 'POST',
         body: formData,
       });
